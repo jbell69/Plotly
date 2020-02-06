c4814b82425ad3a5917e2b7aec972c87ce7d0dd0
Jason Bell <jbell69@mac.com>
2020-02-06 14:43:53 -0600

    version 2

--- static/js/plot.js
+++ static/js/plot.js
@@ -1,44 +1,146 @@
-// Use D3 fetch to read the JSON file
-// The data from the JSON file is arbitrarily named importedData as the argument
-d3.json("data/samples.json").then((importedData) => {
-    // console.log(importedData);
-    var data = importedData;
+function DrawBarGraph(sampleID) {
 
-    // Sort the data array using the greekSearchResults value
-    data.sort(function (a, b) {
-        return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
-    });
+    console.log("DrawBarGraph: Sample: ", sampleID);
 
-    // Slice the first 10 objects for plotting
-    data = data.slice(0, 10);
+    d3.json("static/data/samples.json").then((importedData) => {
 
-    // Reverse the array due to Plotly's defaults
-    data = data.reverse();
+        var data = importedData.samples;
+        var resultArray = data.filter(sampleObj => sampleObj.id == sampleID);
+        var result = resultArray[0];
 
-    // Trace1 for the Greek Data
-    var trace1 = {
-        x: data.map(row => row.greekSearchResults),
-        y: data.map(row => row.greekName),
-        text: data.map(row => row.greekName),
-        name: "Greek",
-        type: "bar",
-        orientation: "h"
-    };
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
 
-    // data
-    var chartData = [trace1];
+        var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
 
-    // Apply the group bar mode to the layout
-    var layout = {
-        title: "Greek gods search results",
-        margin: {
-            l: 100,
-            r: 100,
-            t: 100,
-            b: 100
+        // Trace1 for the Bar Data
+        var trace1 = {
+            x: sample_values.slice(0, 10).reverse(),
+            y: yticks,
+            text: otu_labels.slice(0, 10).reverse(),
+            name: "OTU",
+            type: "bar",
+            orientation: "h"
         }
-    };
+        // data
+        var chartData = [trace1];
 
-    // Render the plot to the div tag with id "plot"
-    Plotly.newPlot("plot", chartData, layout);
-});
+        // Apply the group bar mode to the layout
+        var layout = {
+            title: "Top 10 Bacterial Specimens",
+            margin: { t: 30, l: 150 }
+        }
+
+        // Render the plot to the div tag with id "bar"
+        Plotly.newPlot("bar", chartData, layout);
+    })
+
+}
+
+function DrawBubbleChart(sampleID) {
+
+    console.log("DrawBubbleChart: Sample: ", sampleID);
+
+    d3.json("static/data/samples.json").then((importedData) => {
+
+        var data = importedData.samples;
+        var resultArray = data.filter(sampleObj => sampleObj.id == sampleID);
+        var result = resultArray[0];
+
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+
+
+        // Trace2 for the Bubble Data
+        var trace2 = {
+            x: otu_ids,
+            y: sample_values,
+            text: otu_labels,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids,
+                colorscale: "Earth"
+            }
+        }
+        // data
+        var bubbleData = [trace2];
+
+        // Apply the group bar mode to the layout
+        var bubblelayout = {
+            title: "Bacteria Cultures Per Sample",
+            margin: { t: 0 },
+            hovermode: "closest",
+            xaxis: { title: "OTU ID" },
+            margin: { t: 30 }
+        }
+
+        // Render the plot to the div tag with id "bar"
+        Plotly.newPlot("bubble", bubbleData, bubblelayout);
+    })
+
+}
+
+function ShowMetaData(sampleID) {
+
+    console.log("ShowMetaData: Sample: ", sampleID);
+
+    d3.json("static/data/samples.json").then((importedData) => {
+
+        var metadata = importedData.metadata;
+
+        var resultArray = metadata.filter(sampleObj => sampleObj.id == sampleID);
+        var result = resultArray[0];
+
+        var PANEL = d3.select("#sample-metadata");
+
+        PANEL.html("");
+
+        Object.entries(result).forEach(([key, value]) => {
+            var textToShow = `${key.toUpperCase()}: ${value}`;
+            PANEL.append("h6").text(textToShow);
+        });
+    });
+}
+
+function optionChanged(newSampleID) {
+
+    console.log("Dropdown changed to: ", newSampleID);
+
+    ShowMetaData(newSampleID);
+    DrawBarGraph(newSampleID);
+    DrawBubbleChart(newSampleID);
+
+}
+
+function Init() {
+
+    console.log("Initializing Screen");
+
+    // Populate the Dropdown with all sample ID's
+    var selector = d3.select("#selDataset");
+
+    d3.json("static/data/samples.json").then((SampleData) => {
+        // console.log(importedData);
+        var SampleName = SampleData.names;
+
+        SampleName.forEach((sample) => {
+            selector
+                .append("option")
+                .text(sample)
+                .property("value", sample);
+        });
+
+        var sample = SampleName[0];
+
+        ShowMetaData(sample);
+        DrawBarGraph(sample);
+        DrawBubbleChart(sample);
+
+    });
+}
+
+Init();
+
